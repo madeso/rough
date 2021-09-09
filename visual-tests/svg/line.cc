12bbc4f88665bdf95b42e0a6df60019d70fb5359
Gustav <sir.gustav.the.coder@gmail.com>
2021-09-09 23:14:37 +0200

    test: moved line so it isn't drawn ontop of another line (that's weird)

--- visual-tests/svg/line.cc
+++ visual-tests/svg/line.cc
@@ -6,7 +6,7 @@ svg::Document line_test()
   rc.line(10, 10, 100, 10);
   rc.line(10, 210, 500, 210);
   rc.line(10, 20, 10, 110, Options{}.set_stroke("red"));
-  rc.line(10, 10, 100, 10);
+  rc.line(10, 150, 100, 150);
   rc.line(50, 30, 200, 100, Options{}.set_stroke("blue").set_strokeWidth(5));
 
   return rc.doc;
